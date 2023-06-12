7bfb6e62300a8fa14d34467a44b381fdb03a0083
Amanuel Belete <amanuelbelete95@gmail.com>
2023-06-12 20:02:39 +0300

    Create an app for cart update

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Navbar from './components/Navbar';
+
+import CartContainer from './components/CartContainer';
+
+function App() {
+  return (
+    <main className="App">
+      <Navbar/>
+      <CartContainer/>
+    </main>
+  );
+}
+
+export default App;
